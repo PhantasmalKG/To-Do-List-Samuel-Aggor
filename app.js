3f982b1e176ec28d7e8b7167328649867bfc5146
Samuel Aggor <aggorcode@yahoo.com>
2024-04-07 18:49:45 -0400

    Redid App UI
    Did some thinking and overhauled the UI for the app. The app looks cleaner, but some sizing issues remain.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-
+window.addEventListener('load', () => {
+    const entryForm = document.querySelector("new-entry-form");
+    const input = document.querySelector("#new-entry-input");
+    const entryLog = document.querySelector("#")
+})
 let todoNotes = [];
 
 function addTask(){
